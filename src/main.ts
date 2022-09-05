8421cc18b52901599206d3f0e95f07fedb4ed4e0
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2022-09-05 09:04:43 -0400

    Debugging

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
-import { execSync } from "child_process";
+import { exec } from "child_process";
 import path from "path";
+import core from "@actions/core";
 
 const ENTRYPOINT_SCRIPT_PATH: string = path.join(__dirname, "../entrypoint.sh");
 
 async function run(): Promise<void> {
-  execSync(`bash ${ENTRYPOINT_SCRIPT_PATH}`, { env: process.env });
+  core.info("Running entrypoint script: " + ENTRYPOINT_SCRIPT_PATH);
+  exec(`bash ${ENTRYPOINT_SCRIPT_PATH}`, { env: process.env }, (err, stdout, stderr) => {
+    if (err) {
+      //some err occurred
+      console.error(err)
+    } else {
+     // the *entire* stdout and stderr (buffered)
+     console.log(`stdout: ${stdout}`);
+     console.log(`stderr: ${stderr}`);
+    }
+  });
 }
 
 run();
