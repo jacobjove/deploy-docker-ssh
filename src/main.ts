903d102539dae1193df01fa66ca52047eb3821d6
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2022-10-11 22:46:43 -0400

    Bug fix

--- src/main.ts
+++ src/main.ts
@@ -5,24 +5,16 @@ import * as core from "@actions/core";
 import * as fs from "fs";
 import { nanoid } from "nanoid";
 
-function execInRealTime(
-  ...args: Parameters<typeof execSync>
-): ReturnType<typeof execSync> {
-  const [command, options] = args;
-  return execSync(command, {
-    shell: "/bin/bash",
-    stdio: "inherit",
-    ...(options ?? {}),
-  });
-}
-
 async function run(): Promise<void> {
+  // Required the SSH agent to be configured.
   if (!process.env.SSH_AUTH_SOCK) {
     core.setFailed(
       `SSH agent is not initialized. Please use the ssh-agent action: https://github.com/webfactory/ssh-agent`
     );
     return;
   }
+
+  // Verify workspace structure.
   const GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE;
   if (!GITHUB_WORKSPACE) {
     core.setFailed("GITHUB_WORKSPACE is not set.");
@@ -31,18 +23,13 @@ async function run(): Promise<void> {
     core.setFailed(`${GITHUB_WORKSPACE} does not exist.`);
     return;
   }
-  process.chdir(GITHUB_WORKSPACE);
+
+  // Read inputs.
   const inputs: Inputs = await getInputs();
-  // Instead of initializing the SSH agent here, use the ssh-agent action:
-  // https://github.com/webfactory/ssh-agent
-  // core.info("Adding GitHub to known hosts...");
-  // execInRealTime("mkdir -p ~/.ssh");
-  // execInRealTime(`ssh-agent -a "${inputs.sshAuthSock}" > /dev/null`);
-  // execInRealTime(`ssh-keyscan github.com >> ~/.ssh/known_hosts`);
-  // execInRealTime(`ssh-add - <<< "${inputs.sshPrivateKey}"`);
-  const distDirPath = path.join(GITHUB_WORKSPACE, `tmp-${nanoid()}`);
-  fs.mkdirSync(distDirPath, { recursive: true });
+
   const sshPartial = `ssh -o StrictHostKeyChecking=no -p "${inputs.sshPort}"`;
+
+  // Confirm the target directory exists on the server.
   core.info("Confirming target directory exists on remote server...");
   const successMessage = "Confirmed target directory exists.";
   const targetDirCheckOutput = execSync(
@@ -56,12 +43,24 @@ async function run(): Promise<void> {
     core.setFailed(targetDirCheckOutput);
     return;
   }
+
   if (inputs.files) {
-    const filepaths = inputs.files
-      .split(/[\s\n]+/)
-      .map((filepath) => path.join(inputs.sourceDir, filepath));
-    core.info(`To be transported:\n${filepaths.join("\n")}`);
-    for (const filepath of filepaths) {
+    // Generate a temporary directory to hold only the deployable files.
+    const distDirPath = path.join(GITHUB_WORKSPACE, `tmp-${nanoid()}`);
+    fs.mkdirSync(distDirPath, { recursive: true });
+
+    // Enter the source directory.
+    const sourceDir = path.join(GITHUB_WORKSPACE, inputs.sourceDir);
+    if (!fs.existsSync(sourceDir)) {
+      core.setFailed(`${sourceDir} does not exist.`);
+      return;
+    }
+    process.chdir(sourceDir);
+
+    // Copy the deployable files from the source directory to the temporary directory.
+    const relativeFilepaths = inputs.files.split(/[\s\n]+/);
+    core.info(`To be transported:\n${relativeFilepaths.join("\n")}`);
+    for (const filepath of relativeFilepaths) {
       if (!fs.existsSync(filepath)) {
         core.setFailed(`${filepath} does not exist.`);
         return;
@@ -72,6 +71,8 @@ async function run(): Promise<void> {
     }
     core.info(`Prepared distribution directory with the following contents:`);
     execInRealTime(`ls -a ${distDirPath}`);
+
+    // Sync the temporary directory to the target directory on the server.
     core.info(
       `Syncing distribution directory to ${inputs.host}:${inputs.targetDir} ...`
     );
@@ -79,6 +80,8 @@ async function run(): Promise<void> {
       `rsync -rPv -e "${sshPartial}" "${distDirPath}/" "${inputs.user}@${inputs.host}:${inputs.targetDir}"`
     );
   }
+
+  // Execute the remote command.
   core.info(`Starting SSH connection with ${inputs.host} ...`);
   const command = `cd '${inputs.targetDir}' && ${inputs.command}`;
   core.info(command);
@@ -92,3 +95,14 @@ async function run(): Promise<void> {
 }
 
 run();
+
+function execInRealTime(
+  ...args: Parameters<typeof execSync>
+): ReturnType<typeof execSync> {
+  const [command, options] = args;
+  return execSync(command, {
+    shell: "/bin/bash",
+    stdio: "inherit",
+    ...(options ?? {}),
+  });
+}
