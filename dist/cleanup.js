563cbdd0b5195cfb90d73d6d7f6986b7b1130e0c
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2022-10-14 21:50:59 -0400

    debug

--- dist/cleanup.js
+++ dist/cleanup.js
@@ -1,13 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/no-require-imports */
 /* eslint-disable import/no-commonjs */
-const core = require("@actions/core");
 const { execSync } = require("child_process");
 
 try {
   // Kill the started SSH agent
-  core.info("Stopping SSH agent...");
   execSync("ssh-agent -k");
 } catch (error) {
-  core.info(String(error));
+  console.log(String(error));
 }
